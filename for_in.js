3a1f57dbe1e15205b947fe93247c3b8c5f562060
Jitesh-Bharad <jitesh.bharad0@gmail.com>
2023-03-06 21:40:06 +0530

    Add files via upload

---
+++ for_in.js
@@ -0,0 +1,16 @@
+
+
+let x=[12,1,90,100,89];
+
+for(let val of x)
+{
+	console.log(val);
+}
+
+let obj={name:'ramesh',rollno:'cs1',marks:100};
+
+for(let k in obj)
+{
+	console.log(k);
+	console.log(obj[k]);
+}
